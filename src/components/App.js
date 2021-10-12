4928d5ed1a642aaffcf11f6092b94627136fbe60
Stephanie Cushing <slcushing@gmail.com>
2021-10-12 16:54:50 -0400

    refactors order post request

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import Order from './Order'
 import basilImage from './images/basil.jpeg';
 
 
-const ORDER_URL = "https://django-restaurantapi-slcushing.herokuapp.com/api_v1/orders/";
+
 //order endpoint that I set up using django/heroku
 
 
@@ -35,8 +35,8 @@ function App() {
   }, []);
 
   async function addOrder(order, name, phoneNumber) {
-    const newOrder = {order, name, phoneNumber};
-    const response = await fetch(`${ORDER_URL}`, {
+    const newOrder = {order, name, phone_number: phoneNumber};
+    const response = await fetch('https://django-restaurantapi-slcushing.herokuapp.com/api_v1/orders/', {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -45,23 +45,23 @@ function App() {
   });
 
     if (response.ok) {
-      const data = await response.json();
-      setOrder([...order, data]);
+  //     const data = await response.json();
+      console.log("Thanks for your order")
     }
   }
 
-  const [selection, setSelection] = useState('appetizer')
+  // const [selection, setSelection] = useState('appetizer')
   
   
-  if (selection === 'appetizer') {
-    menuItems.catgory = "Appetizer"
-  } else if (selection === 'entrees') {
-    menuItems.category = "Entree"
-  } else if (selection === 'desserts') {
-    menuItems.category = "Dessert"
-  } else if (selection === 'drinks') {
-    menuItems.category = "Drink"
-  };
+  // if (selection === 'appetizer') {
+  //   menuItems.catgory = "Appetizer"
+  // } else if (selection === 'entrees') {
+  //   menuItems.category = "Entree"
+  // } else if (selection === 'desserts') {
+  //   menuItems.category = "Dessert"
+  // } else if (selection === 'drinks') {
+  //   menuItems.category = "Drink"
+  // };
 
 
 
@@ -80,7 +80,7 @@ function App() {
         <h1>Nonna's Kitchen</h1>
       </header>
       <nav className="category-bar">
-        <button type="submit" className="appetizer" onClick={() => setSelection("Appetizer")}>Appetizers</button>
+        <button type="submit" className="appetizer">Appetizers</button>
         <button type="submit" className="entrees">Entrees</button>
         <button type="sumbit" className="desserts">Desserts</button>
         <button type="submit" className="drinks">Drinks</button>
